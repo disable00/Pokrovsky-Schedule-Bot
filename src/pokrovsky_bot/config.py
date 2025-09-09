0d50d9ce54f9830f53792a331960b324e6bfe580
disable00 <108535065+disable00@users.noreply.github.com>
2025-09-10 00:30:58 +0700

    Update config.py

--- src/pokrovsky_bot/config.py
+++ src/pokrovsky_bot/config.py
@@ -11,10 +11,10 @@ class Settings:
     BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
     ADMIN_ID: int = int(os.getenv("ADMIN_ID", "0"))
     NEWS_CHANNEL_URL: str = os.getenv("NEWS_CHANNEL_URL", "https://t.me/YourNewsChannel")
+    NEWS_CHANNEL_ID = os.getenv("NEWS_CHANNEL_ID", "")
     PAGE_URL: str = os.getenv("PAGE_URL", "https://pokrovsky.gosuslugi.ru/glavnoe/raspisanie/")
     DB_PATH: str = os.getenv("DB_PATH", "bot_stats.sqlite3")
     TZ: str = os.getenv("TZ", "Europe/Moscow")
-
     USER_AGENT: str = "ScheduleBot/1.0"
 
 
